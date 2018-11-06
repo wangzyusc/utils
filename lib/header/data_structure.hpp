a2d03a4c898d9f707b13984a4b868103f1354485
Zhiyuan Wang <wangzyzju@gmail.com>
2018-11-05 23:18:48 -0500

    Move struct from io to ds

--- lib/header/data_structure.hpp
+++ lib/header/data_structure.hpp
@@ -1,9 +1,31 @@
 #ifndef __UTILS_DS_
 #define __UTILS_DS_
 
+#include <string>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
+
+//Some basic data structure declaration
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
 class MaxHeap {
 public:
--- lib/header/io.hpp
+++ lib/header/io.hpp
@@ -1,33 +1,11 @@
 #ifndef __UTILS_IO_
 #define __UTILS_IO_
 
-#include <string>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <list>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
+#include "data_structure.hpp"
 #include <iostream>
 #include <sstream>
 
 using namespace std;
-
-//Some basic data structure declaration
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 //Vector I/O
 template<typename T>
